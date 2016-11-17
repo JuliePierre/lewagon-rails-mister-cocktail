3b09728ffc094260e9c55a49ae7503d3a0b692b0
Julie Pierre <julie.pierre3@gmail.com>
2016-11-17 14:21:17 +0100

    added the delete route

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,5 +1,6 @@
 class DosesController < ApplicationController
-  before_action :find_cocktail, only: [:new, :create]
+  before_action :find_cocktail, only: [:new, :create, :destroy]
+
   def new
     @dose = Dose.new
     @ingredients = Ingredient.all
@@ -8,6 +9,12 @@ class DosesController < ApplicationController
   def create
     @dose = @cocktail.doses.build(dose_params)
     @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
     redirect_to cocktail_path(@cocktail)
   end
 
